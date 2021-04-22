8ecd4464c1e5e21d82c00a90f7751792d95c0d3b
isaacfallas <isaacfallas6@gmail.com>
2021-04-21 18:02:16 -0600

    region proposal detection

---
+++ region_proposal_detection/region_proposal_detection.py
@@ -0,0 +1,84 @@
+# import the necessary packages
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import img_to_array
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import cv2
+
+def selective_search(image):
+    ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+    ss.setBaseImage(image)
+    ss.switchToSelectiveSearchFast()
+    rects = ss.process()
+    return rects
+
+labelsFilter = ["ant"]
+
+minProb = 0.2
+
+print("loading ResNet50 model...")
+
+model = ResNet50(weights="imagenet")
+
+print("loading image...")
+
+image = cv2.imread('../images/ants.jpg')
+
+(H, W) = image.shape[:2]
+
+rects = selective_search(image)
+
+proposals = []
+boxes = []
+
+print(len(rects))
+
+for (x, y, w, h) in rects:
+
+    if h/float(H) < 0.08 or w/float(W) < 0.08:
+        continue
+
+    if h/float(H) > 0.8 or w/float(W) > 0.8:
+        continue 
+
+    roi = image[y:y+h, x:x+w]
+    roi = cv2.cvtColor(roi,cv2.COLOR_BGR2RGB)
+    roi = cv2.resize(roi, (224,224))
+
+    roi = img_to_array(roi)
+    roi = preprocess_input(roi)
+
+    proposals.append(roi)
+    boxes.append((x,y,w,h))
+
+#print(len(proposals))
+#print(len(boxes))
+
+proposals = np.array(proposals)
+
+preds = model.predict(proposals)
+preds = imagenet_utils.decode_predictions(preds, top=1)
+
+labels = {}
+
+#print(preds)
+
+for (i, p) in enumerate(preds):
+    
+    (imageID, label, prob) = p[0]
+    
+    if labelFilters is not None and label not in labelFilters:
+        continue
+
+    if prob >= minProb:
+        (x, y, w, h) = boxes[i]
+        box = (x, y, x+w, y+h)
+
+        L = labels.get(label,[])
+        L.append((box , prob))
+        labels[label] = L
+
+    
